716f1bdf104447c6221da55995759d4db1b225a9
sinegia <endibro75@gmail.com>
2022-09-16 12:14:36 +0300

    first commit

---
+++ GotCalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c;
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Результат: " + c);
+            Console.ReadKey(true);
+
+        }
+    }
+}
